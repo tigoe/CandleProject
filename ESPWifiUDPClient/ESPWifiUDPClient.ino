fa481778a49cd297acb5dff5694c04583dd39d5e
tigoe <tom.igoe@gmail.com>
2015-07-26 18:29:55 -0400

    Updaged UDP client

--- ESPWifiUDPClient/ESPWifiUDPClient.ino
+++ ESPWifiUDPClient/ESPWifiUDPClient.ino
@@ -26,7 +26,10 @@ WiFiUDP Udp;
 const int port = 8888;
 byte mac[6];
 long tenMinutes = 1000 * 60 * 10;
+int loginInterval = 10 * 1000;
 long lastNetworkMsg = 0;
+long lastLogin = 0;
+boolean online = false;
 
 void setup() {
   pinMode(5, OUTPUT);
@@ -63,6 +66,7 @@ void loop() {
       Serial.print(line);
     }
     lastNetworkMsg = millis();
+    online = true;
   }
 
   // Read all the lines of the reply from server and print them to Serial
@@ -73,22 +77,25 @@ void loop() {
     Udp.println(line);
     Udp.endPacket();
   }
-
-  if (millis() % tenMinutes < 5) {
+// if you've never gotten a server message,
+// continue to try to login every ten seconds:
+  if (!online && (millis() - lastLogin > loginInterval)) {
     login();
   }
 
   // if you haven't gotten a server message in ten minutes, assume
   // the server's offline and let the candle controller know:
-  if (millis() - lastNetworkMsg > tenMinutes) {
+  if (online && millis() - lastNetworkMsg > tenMinutes) {
     Serial.println("~~~");
+    login();
+    online = false;
   }
 }
 
 
 boolean login() {
+  Serial.println("logging in...");
   Udp.begin(port);
-
   // This will send the MAC address to the server:
   WiFi.macAddress(mac);             //get your MAC address
   String macAddr;                   // a string to convert it for sending
@@ -102,4 +109,5 @@ boolean login() {
   Udp.beginPacket(host, 8888);
   Udp.println(macAddr);
   Udp.endPacket();
+  lastLogin = millis();
 }
