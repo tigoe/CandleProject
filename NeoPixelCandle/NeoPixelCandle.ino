f3bddba8fdc227b568e6f3a57c76f3f44dc13ea4
tigoe <tom.igoe@gmail.com>
2020-06-06 09:17:53 -0400

    Update NeoPixelCandle.ino

--- NeoPixelCandle/NeoPixelCandle.ino
+++ NeoPixelCandle/NeoPixelCandle.ino
@@ -3,26 +3,25 @@
   This sketch makes three NeoPixels fade in a candle-like behavior.
 
   created 26 Jun 2015
-  modified 24 Jul 2015
+  modified 10 Oct 2019
   by Tom Igoe
 
- */
-//#include <CapacitiveSensor.h>
-//#include <SoftwareSerial.h>
+*/
+#include <CapacitiveSensor.h>
+#include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
 
-const int neoPixelPin = 6;
-const int numPixels = 12;
+const int neoPixelPin = 0;
+const int numPixels = 3;
 
-//SoftwareSerial mySerial(3, 4); // RX, TX
+SoftwareSerial mySerial(3, 4); // RX, TX
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, neoPixelPin, NEO_GRB + NEO_KHZ800);
 
-//CapacitiveSensor  mySensor = CapacitiveSensor(1, 2);       // 10M resistor between pins 1 & 2, pin 2 is sensor pin
+CapacitiveSensor  mySensor = CapacitiveSensor(1, 2);       // 10M resistor between pins 1 & 2, pin 2 is sensor pin
 
 // changing range of keyframe colors for the pixels to flicker to:
-//unsigned long keyColors[] = {0xCB500F, 0xB4410C, 0x95230C, 0x853E0B};
-unsigned long keyColors[] = {0x2300DE, 0x1512ED, 0x0034D9, 0x2315FF};
+unsigned long keyColors[] = {0xCB500F, 0xB4410C, 0x95230C, 0x853E0B};
 
 unsigned long targetColor[numPixels];    // next target for each pixel
 unsigned long pixelColor[numPixels];     // current color for each pixel
@@ -38,44 +37,42 @@ long lastTwinkle = 0;         // how long since the last local twinkle
 long lastNetworkTwinkle = 0;  // how long since the last network twinkle
 
 void setup()  {
- // mySerial.begin(9600);                  // initialize serial
-//  mySensor.set_CS_Timeout_Millis(50);
-//  mySensor.set_CS_Timeout_Millis(50);
+  mySerial.begin(9600);                  // initialize serial
+  mySensor.set_CS_Timeout_Millis(50);
   strip.begin();                          // initialize pixel strip
-  for (int pixel = 0; pixel < numPixels; pixel++) {
-    strip.setPixelColor(pixel, 0, 0, 0);  // turn off pixel
-    strip.show();                         // refresh the strip
-  }
+  strip.clear();  // turn off pixels
+  strip.show();                         // refresh the strip
+
 
   delay(1000);    // delay to see the LEDs reset before main loop
 }
 
 void loop() {
   // if you get a serial x, make with the twinkling:
-//  if (mySerial.available()) {
-//    readSerial();
-//  }
+  if (mySerial.available()) {
+    readSerial();
+  }
 
   // create the flicker effect:
   if (millis() % flickerInterval < 2) {
     flickerPixels();
   }
 
-//  if (!online) {
-//    strip.setPixelColor(0, 0, 0, 34);  // turn first pixel to low blue
-//  }
+  if (!online) {
+    strip.setPixelColor(0, 0, 0, 34);  // turn first pixel to low blue
+  }
 
   // read sensor every half second:
   if (millis() - lastTwinkle > sensingInterval) {       // disable the sensor after a sensing event happens
-//    long touch =  mySensor.capacitiveSensor(15);
-//    if (touch > threshold) {
-//      // if the minimum network interval has passed:
-//      if (millis() - lastNetworkTwinkle > networkInterval) {
-//        mySerial.print("*");
-//        lastNetworkTwinkle = millis();
-//      }
-//      twinkle();
-//    }
+    long touch =  mySensor.capacitiveSensor(15);
+    if (touch > threshold) {
+      // if the minimum network interval has passed:
+      if (millis() - lastNetworkTwinkle > networkInterval) {
+        mySerial.print("*");
+        lastNetworkTwinkle = millis();
+      }
+      twinkle();
+    }
   }
   // update the strip:
   strip.show();
@@ -83,24 +80,24 @@ void loop() {
 
 /*
   this function reads serial input and interprets it
- */
+*/
 
-//void readSerial() {
-//  char input = mySerial.read();
-//  switch (input) {
-//    case '*':    // do a twinkle
-//      twinkle();
-//      break;
-//    case '!':
-//      online = true;
-//      break;
-//    case '~':
-//      online = false;
-//      break;
-//    default:  // placeholder for other options here
-//      break;
-//  }
-//}
+void readSerial() {
+  char input = mySerial.read();
+  switch (input) {
+    case '*':    // do a twinkle
+      twinkle();
+      break;
+    case '!':
+      online = true;
+      break;
+    case '~':
+      online = false;
+      break;
+    default:  // placeholder for other options here
+      break;
+  }
+}
 
 /*
   this function creates the twinkle effect:
@@ -159,4 +156,3 @@ unsigned long compare(unsigned long thisColor, unsigned long thatColor) {
   unsigned long result = ((unsigned long)r << 16) | ((unsigned long)g << 8) | b;
   return result;
 }
-
